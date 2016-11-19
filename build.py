d58860098e8c5a025894eff535180e5a24e743ac
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-19 14:32:34 +0530

    Initial commit
    Header of trianglify® of iamprabhat® | Build: SEED™ — Umeå.
    Copyright © 2008 - 2016, Prabhat Kumar, All rights reserved.
    Copyright © 2014 - 2016, Sequømics Corporation, All rights reserved.
    URL: http://prabhatkumar.org/ & http://sequomics.com/.

--- build.py
+++ build.py
@@ -30,3 +30,27 @@ import tempfile
 from termcolor import colored
 
 localtime = time.asctime(time.localtime(time.time()))
+
+header ='''/*!
+ * ┌─────────────────────────────────────────────────────────────────────────┐
+ * | trianglify® of iamprabhat®                                              |
+ * ├─────────────────────────────────────────────────────────────────────────┤
+ * | From the Desk of Prabhat Kumar — CEO, Founder & Scientist.              |
+ * ├─────────────────────────────────────────────────────────────────────────┤
+ * | Copyright © 2008 - 2016, Prabhat Kumar, All rights reserved.            |
+ * └─────────────────────────────────────────────────────────────────────────┘
+ * ---------------------------------------------------------------------------
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 18-Nov-2016
+ * @license   : Apache, version 2.0
+ * @require   : d3.js® & Raphaël.js®
+ * @build     : SEED™ — Umeå
+ *              |---- A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ */
+'''
