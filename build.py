cb3b10c655092715e1cc9f537fedcc8dee788a86
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-19 09:44:08 +0530

    Initial commit
    Copyright © 2006 - 2016, Prabhat Kumar. All rights reserved.

--- build.py
+++ build.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# ————————————————————————————————————————————————————————————————————————————
+# Copyright © 2006 - 2016, Prabhat Kumar. All rights reserved.
+# ————————————————————————————————————————————————————————————————————————————
+# Licensed under the Apache License (the "License, version 2.0");
+# you may not use this file except in compliance with the License.
+# ————————————————————————————————————————————————————————————————————————————
