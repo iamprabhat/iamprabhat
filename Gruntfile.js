26857410c3a9210c54e3445132a0b41f628bd682
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-19 09:58:01 +0530

    Initial commit
    SEED™: Build System — Umeå.
    A Sequømics Product — http://sequomics.com/.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,29 @@
 #!/usr/bin/env node
+
+
+/*!
+ * iamprabhat®
+ * From the Desk of Prabhat Kumar — CEO, Founder & Scientist.
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 18-Nov-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-cli
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "iamprabhat®".
+ * ___________________________________________________________________________
+ */
