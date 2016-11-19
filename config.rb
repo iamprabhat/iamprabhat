50e82866e0df995393bb5d2d9894283f77bb5e2a
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-19 09:50:51 +0530

    Initial commit
    --/The Heart of Beauty/-- of http://prabhatkumar.org/.

--- config.rb
+++ config.rb
@@ -1,1 +1,20 @@
 #!/usr/bin/env ruby
+
+
+# iamprabhat®
+# -----------------------------------------
+#        With ♥ — by Prabhat Kumar.
+# -----------------------------------------
+# @author     : Prabhat Kumar.
+# @copyright  : Prabhat Kumar.
+# @homepage   : http://prabhatkumar.org/.
+# @require    : Ruby
+# @require    : Sass
+# @require    : Compass
+# @version    : 1.0.0
+# @build      : Umeå
+# @date       : 18-Nov-2016
+# @license    : Apache, version 2.0
+# -----------------------------------------
+# Principle   : Standard Module Definition
+# -----------------------------------------
