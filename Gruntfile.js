a6a338ff7f28876c19ace2f0f628f977282780f6
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-20 11:26:34 +0530

    Initial commit
    Reading {./secret.json} from the —//root//— to get credential(s) information.

--- Gruntfile.js
+++ Gruntfile.js
@@ -113,3 +113,9 @@ var bkg  = JSON.parse(fs.readFileSync('./bower.json', {
 var pkg  = JSON.parse(fs.readFileSync('./package.json', {
   encoding: "utf8"
 }));
+
+// To get credential(s) information.
+///---------------------------------
+var ftp  = JSON.parse(fs.readFileSync('./secret.json', {
+  encoding: "utf8"
+}));
