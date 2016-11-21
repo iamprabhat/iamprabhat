8aa2025d736b32fe8a9400134e36c57adafc10ff
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-21 12:16:48 +0530

    Initial commit
    A function to register {hackForCommonJSAndBrowserify}.

--- Gruntfile.js
+++ Gruntfile.js
@@ -212,3 +212,16 @@ var replaceParams = (function() {
     });
   };
 }());
+
+// A function to register `hackForCommonJSAndBrowserify`.
+///------------------------------------------------------
+function hackForCommonJSAndBrowserify(text, sourceMapText) {
+  var dirname = path.dirname(this.outPath);
+  if (!fs.existsSync(dirname)) {
+    fs.mkdirSync(dirname);
+  }
+  text = text.replace(/require/g, 'notrequirebecasebrowserifymessesup');
+  fs.writeFileSync(this.outPath, text, {
+    encoding: "utf-8"
+  });
+}
