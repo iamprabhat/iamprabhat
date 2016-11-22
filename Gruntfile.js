b6facd20c5a25ba6edf8546cbdcae66d2e5308ea
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-22 08:04:49 +0530

    Initial commit
    Defined — A file glowing pattern for JavaScript.

--- Gruntfile.js
+++ Gruntfile.js
@@ -279,3 +279,5 @@ module.exports = function(grunt) {
   // Advanced Grunt API.
   /// -->> A mode defaults to `0777`.
   var mode     = '0777';
+  /// -->> A file globbing pattern.
+  var pattern  = '**/*.js';
