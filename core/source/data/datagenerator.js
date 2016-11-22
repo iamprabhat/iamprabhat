df82a925dd84171f48ce543041c92363016b3a24
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-22 12:58:01 +0530

    Initial commit
    A Grunt task to generate --/Project(s) and Publication(s) Data/-- in to JSON format.
    SEED™ — A Sequømics Product — http://sequomics.com/.

---
+++ core/source/data/datagenerator.js
@@ -0,0 +1,26 @@
+/*!
+ * iamprabhat®
+ * From the Desk of Prabhat Kumar — CEO, Founder & Scientist.
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 22-Nov-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ________________________________________________________________________________
+ *
+ * --/The Heart of Data Build System/-- of "iamprabhat®".
+ * ________________________________________________________________________________
+ */
